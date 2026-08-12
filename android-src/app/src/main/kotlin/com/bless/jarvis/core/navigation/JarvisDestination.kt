1035ba12499f4a6c5b6f6583045c30832b6fc313
blessings2008 <madalitsotambala2@gmail.com>
2026-08-11 22:49:35 -0700

    Add central JARVIS navigation destinations

---
+++ android-src/app/src/main/kotlin/com/bless/jarvis/core/navigation/JarvisDestination.kt
@@ -0,0 +1,8 @@
+package com.bless.jarvis.core.navigation
+
+enum class JarvisDestination(val label: String) {
+    HOME("Home"),
+    CHAT("JARVIS"),
+    ACTIVITY("Activity"),
+    SETTINGS("Settings")
+}
