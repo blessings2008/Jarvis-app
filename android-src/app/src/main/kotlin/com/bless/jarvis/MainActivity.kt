f023d9f7f3c25804ffffe840c27a433183b00ddc
blessings2008 <madalitsotambala2@gmail.com>
2026-08-11 22:50:09 -0700

    Switch MainActivity to modular JARVIS app shell

--- android-src/app/src/main/kotlin/com/bless/jarvis/MainActivity.kt
+++ android-src/app/src/main/kotlin/com/bless/jarvis/MainActivity.kt
@@ -3,10 +3,7 @@ package com.bless.jarvis
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.bless.jarvis.ui.ChatScreen
+import com.bless.jarvis.ui.JarvisApp
 import com.bless.jarvis.ui.theme.JarvisTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,9 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JarvisTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    ChatScreen()
-                }
+                JarvisApp()
             }
         }
     }
